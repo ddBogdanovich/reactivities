9b7096ed0b0e7198535d63c9d3475d3bb06e2949
Eugene Bogdanovich <e.bogdanovich@syberry.com>
2021-07-02 11:20:04 +0300

    End of section 13

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -3,6 +3,7 @@ import { Activity } from './../models/activity';
 import axios, { AxiosError, AxiosResponse } from 'axios';
 import { toast } from 'react-toastify';
 import { store } from '../stores/store';
+import { User, UserFormValues } from '../models/user';
 
 const sleep = (delay: number) => {
     return new Promise((resolve) => {
@@ -49,9 +50,15 @@ axios.interceptors.response.use(async response => {
     return Promise.reject(error);
 })
 
+axios.interceptors.request.use(config => {
+    const token = store.commonStore.token;
+    if(token) config.headers.Authorization = `Bearer ${token}`
+    return config;
+})
+
 const responseBody = <T>(response: AxiosResponse<T>) => response.data;
 
-const request = {
+const requests = {
     get: <T>(url: string) => axios.get<T>(url).then(responseBody), 
     post: <T> (url: string, body: {}) => axios.post<T>(url, body).then(responseBody), 
     put: <T>(url: string, body: {}) => axios.put<T>(url, body).then(responseBody), 
@@ -59,15 +66,22 @@ const request = {
 }
 
 const Activities = {
-    list: () => request.get<Activity[]>('/activities'),
-    details: (id: string) => request.get<Activity>(`/activities/${id}`),
-    create: (activity: Activity) => request.post<void>('/activities', activity),
-    update: (activity: Activity) => request.put<void>(`/activities/${activity.id}`, activity),
-    delete: (id: string) => request.del<void>(`/activities/${id}`)
+    list: () => requests.get<Activity[]>('/activities'),
+    details: (id: string) => requests.get<Activity>(`/activities/${id}`),
+    create: (activity: Activity) => requests.post<void>('/activities', activity),
+    update: (activity: Activity) => requests.put<void>(`/activities/${activity.id}`, activity),
+    delete: (id: string) => requests.del<void>(`/activities/${id}`)
+}
+
+const Account = {
+    current: () => requests.get<User>('/account'),
+    login: (user: UserFormValues) => requests.post<User>('/account/login', user),
+    register: (user: UserFormValues) => requests.post<User>('/account/register', user)
 }
 
 const agent = {
-    Activities
+    Activities,
+    Account
 }
 
 export default agent;
